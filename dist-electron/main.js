48955d0c2f5289442782203b931dabed41fc84dd
AL Loan <AL_Loan@AL-SEMESTER45.ucdenver.pvt>
2024-11-10 19:09:57 -0700

    routing

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,12 +1,32 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./util.js";
-app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    // In development, load from localhost
     if (isDev()) {
-        mainWindow.loadURL("http://localhost:5173");
+        win.loadURL("http://localhost:5173");
     }
     else {
-        mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+        // In production, load the built files
+        win.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+    }
+}
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
     }
 });
