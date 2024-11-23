a0d234b481bf27ecd0eff612ac9eaad7ef40641b
AL Loan <AL_Loan@AL-SEMESTER45.ucdenver.pvt>
2024-11-23 12:37:10 -0700

    base url react deployment

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Search, X } from "lucide-react";
-import data from '@/data/UserData.json';
+import data from '@/Data/UserData.json';
 
 interface User {
   id: string;
