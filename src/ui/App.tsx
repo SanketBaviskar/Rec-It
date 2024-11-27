96b25a03cb6db1230f7626a78689d2654ffae435
Sanket Baviskar <sanketbaviskar01@gmail.com>
2024-11-26 21:23:02 -0700

    update in admin dashboard

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainWindow from '../Pages/MainWindow/MainWindow'
 import Login from "@/Pages/Login/Login";
+import AdminDashboard from "@/Pages/AdminDashboard/AdminDashboard";
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<MainWindow />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
     </Routes>
   </Router>
   );
