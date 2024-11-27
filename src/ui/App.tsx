94d610dde912ad816898e954225c42eb3c8f2e5c
nakulmagotra1322 <nakul.magotra@ucdenver.edu>
2024-11-26 19:38:57 -0700

    Admin

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainWindow from '../Pages/MainWindow/MainWindow'
 import Login from "@/Pages/Login/Login";
-import Admin from "@/Pages/Admin/AdminLayout";
+import Admin from "@/Pages/Admin/AdminLayout";//AdminLayout
 function App() {
 
   return (
