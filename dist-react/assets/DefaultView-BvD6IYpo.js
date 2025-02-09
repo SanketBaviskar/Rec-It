f65f93d95414aa077a79ad971a2e21801eb0752b
Sanket Baviskar <sanketbaviskar01@gmail.com>
2025-02-09 10:22:18 -0700

    updatein build

--- dist-react/assets/DefaultView-BvD6IYpo.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DcC--eh_.js";const r=()=>e.jsx("div",{children:"After clicking options from the sidebar, we will show different components here."});export{r as default};
--- dist-react/assets/MembershipAndPasses-C_YXfU3U.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-DcC--eh_.js";function i(){return s.jsx("div",{children:"this is membership and passes tab"})}export{i as default};
